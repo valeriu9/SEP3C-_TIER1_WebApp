ecfe2b3940cd0b7f22cd15e5f92dd57da1d3de15
valeriu9 <43374180+valeriu9@users.noreply.github.com>
2019-12-11 11:21:31 +0100

    ready to work

---
+++ SEP3_TIER1/wwwroot/css/JScript.js
@@ -0,0 +1,12 @@
+﻿
+    function confirmDelete() {
+        return confirm("Are you sure that you want to delete this Flight Plan?");
+    };
+    function refresh() {
+        location.reload();
+    }
+
+ type="text/javascript">
+        $(function () {
+            $('#datetimepicker1').datetimepicker();
+    });
